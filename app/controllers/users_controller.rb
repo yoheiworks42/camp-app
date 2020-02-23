7a661ac847fdf2792eee5c0c768442901aff447d
EC2 Default User <ec2-user@ip-10-0-0-86.ap-northeast-1.compute.internal>
2020-02-23 08:32:51 +0000

    マイナーアップデート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -18,15 +19,46 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success]="アカウントを削除しました"
+    redirect_to root_url
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name,:email,:password, :password_confirmation)
   end
   
-  def destroy
-    log_out
-    redirect_to root_url
+  # beforeアクション
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
-  
+=begin
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+=end
 end
