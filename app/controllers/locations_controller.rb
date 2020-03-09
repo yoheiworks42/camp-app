9dc5ae3988f0c57dd4a12ebd978f08f5a649ddbe
EC2 Default User <ec2-user@ip-10-0-0-86.ap-northeast-1.compute.internal>
2020-03-09 11:31:15 +0000

    マイナーチェンジ

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -16,7 +16,7 @@ class LocationsController < ApplicationController
   def new
     @location = Location.new
   end
-  
+
   def create
     @location = Location.new(location_params)
     if @location.save
