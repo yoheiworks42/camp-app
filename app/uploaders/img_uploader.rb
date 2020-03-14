25cee1909f88914ed8c8af75692cb4e9dbf94820
EC2 Default User <ec2-user@ip-10-0-0-86.ap-northeast-1.compute.internal>
2020-03-14 07:19:44 +0000

    微修正

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -17,12 +17,11 @@ class ImgUploader < CarrierWave::Uploader::Base
 
 
 #上限変更　showでプレビューされるサイズ
-  process resize_to_limit: [300, 200]
-  
+  process resize_to_limit: [600, 400]
 
 #サムネイルを生成
   version :thumb do
-    process :resize_to_limit => [300, 300]
+    process :resize_to_limit => [300, 200]
   end
 
 
