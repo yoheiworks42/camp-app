29b90c5cd6e60edb25b321e82e99182640d7ef91
EC2 Default User <ec2-user@ip-10-0-0-86.ap-northeast-1.compute.internal>
2020-04-03 23:50:00 +0000

    0404-01

--- config/routes.rb
+++ config/routes.rb
@@ -8,22 +8,24 @@ Rails.application.routes.draw do
   post "/login",to:'sessions#create'
   delete "/logout",to:'sessions#destroy'
   get "/users", to:'users#new'
-  get "/users/:id/edit",to:'users#edit'
-  post "/users/:id/edit",to:"users#update"
+  #get "/users/:id/edit",to:'users#edit'
+  #post "/users/:id/edit",to:"users#update"
 
   resources :users
 
-  get "/test",to:"locations#test"
+  #get "/test",to:"locations#test"
   get "/index",to:'locations#index'
-  get "/detail",to:'locations#show'
+  get "/locations/:id",to:'locations#show'
   get "/locations",to:'locations#new'
   post "/locations",to:'locations#create'
   get "/map",to:'locations#map'
-  resources :locations
+  #resources :locations
 
-  get "/locations/:id/post",to:"posts#new"
-  post "/locations/:id/post",to:"posts#create"
-  get "/hoge",to:"posts#hoge"
-  resources :posts
+  #get "/locations/:id/post",to:"posts#new"
+  #post "/locations/:id/post",to:"posts#create"
+  resources :locations do
+   resources :posts
+  end
+
 
 end
